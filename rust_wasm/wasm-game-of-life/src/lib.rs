1dfe7b4916bb48724e2ad7b2fd81a70cb3fb9602
Yannis <kessas.belhadj@gmail.com>
2023-04-19 00:01:27 +0200

    Rust and web assembly

---
+++ rust_wasm/wasm-game-of-life/src/lib.rs
@@ -0,0 +1,22 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)] //Import console.log built-in JS function
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("{} greet you 🌍", name));
+    log(&format!("{} salute you from here aswell 👨🏻‍💻", name));
+}
