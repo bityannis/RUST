dc393edf7fce23a757a95676947dd994d3bdfe24
Yannis <kessas.belhadj@gmail.com>
2023-04-19 12:01:34 +0200

    adding first animation on canva from game of life

--- rust_wasm/wasm-game-of-life/pkg/wasm_game_of_life.d.ts
+++ rust_wasm/wasm-game-of-life/pkg/wasm_game_of_life.d.ts
@@ -4,3 +4,25 @@
 * @param {string} name
 */
 export function greet(name: string): void;
+/**
+*/
+export enum Cell {
+  Dead = 0,
+  Alive = 1,
+}
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+*/
+  tick(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+* @returns {string}
+*/
+  render(): string;
+}
